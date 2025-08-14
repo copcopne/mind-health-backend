ec1bacf7787216dcbd3375cd4e5da5175fbb8ae7
Si Nguyen <copcopne@gmail.com>
2025-08-14 20:38:38 +0700

    init

---
+++ src/main/java/com/si/mindhealth/MindhealthApplication.java
@@ -0,0 +1,13 @@
+package com.si.mindhealth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MindhealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MindhealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/si/mindhealth/MindhealthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.si.mindhealth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindhealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
